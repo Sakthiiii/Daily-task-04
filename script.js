ed3dfeb3aaf08a8617abf57fc06a0a6508cf6a40
Sakthiiii <87663226+Sakthiiii@users.noreply.github.com>
2021-08-09 08:26:54 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+var request=new XMLHttpRequest ();
+request .open('GET','https://restcountries.eu/rest/v2/all',true);
+request.send ();
+request.onload =function (){
+     var data =JSON.parse(this.response);
+     console.log(data);
+
+     for (var i=0;i<250;i++){
+         console.log (data[i].name+" " +data[i].flag);
+     }
+    
+}
+
+
+
+
